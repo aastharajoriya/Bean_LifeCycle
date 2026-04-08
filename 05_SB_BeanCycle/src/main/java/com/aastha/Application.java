cdeea892274dbce52417e24eabfc8233d31b82b6
Aastha <136860297+aastharajoriya@users.noreply.github.com>
2026-04-08 10:19:58 -0400

    Add files via upload

---
+++ 05_SB_BeanCycle/src/main/java/com/aastha/Application.java
@@ -0,0 +1,22 @@
+package com.aastha;
+
+import com.aastha.beans.Car;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		ConfigurableApplicationContext context =SpringApplication.run(Application.class, args);
+
+
+		Car bean=context.getBean(Car.class);
+		bean.drive();
+		context.close();
+
+	}
+
+}
